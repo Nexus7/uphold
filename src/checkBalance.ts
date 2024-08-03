e5f306de048c5afcaba10498e9a1182f96570c5c
Rick Behl <rickbehl@gmail.com>
2024-08-03 09:03:21 +0100

    Remove unused var

--- src/checkBalance.ts
+++ src/checkBalance.ts
@@ -7,9 +7,8 @@ export const checkBalance = async (address: string, chain: string) => {
     if (chain === 'alephium') {
         console.log(chalk.yellow(`Checking balance of ${address} on ${chain} chain`));
         let balanceData: BalanceData = await getAlephiumAddressBalance(address);
-        let rawBalance = balanceData.balance / 1e18;
-        let rawLockedBalance = balanceData.lockedBalance / 1e18;
-        console.log(`Balance of ${address} on ${chain} chain: ${rawBalance}`);
+        let balance = balanceData.balance / 1e18;
+        console.log(`Balance of ${address} on ${chain} chain: ${balance}`);
     } else {
         console.log(chalk.red(`Chain ${chain} is not supported`));
     }
